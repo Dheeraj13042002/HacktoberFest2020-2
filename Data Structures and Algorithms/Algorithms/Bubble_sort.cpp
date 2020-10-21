34927ff976bedd8c20b32f9374fd334993e44ab3
Dheeraj13042002 <72385490+Dheeraj13042002@users.noreply.github.com>
2020-10-21 23:10:34 +0530

    Update Bubble_sort.cpp

--- Data Structures and Algorithms/Algorithms/Bubble_sort.cpp
+++ Data Structures and Algorithms/Algorithms/Bubble_sort.cpp
@@ -1,17 +1,11 @@
 // Optimized implementation of Bubble sort 
 #include <stdio.h> 
 
-void swap(int *xp, int *yp) 
-{ 
-	int temp = *xp; 
-	*xp = *yp; 
-	*yp = temp; 
-} 
-
 // An optimized version of Bubble Sort 
 void bubbleSort(int arr[], int n) 
 { 
 int i, j; 
+int temp;	
 bool swapped; 
 for (i = 0; i < n-1; i++) 
 { 
@@ -19,9 +13,11 @@ for (i = 0; i < n-1; i++)
 	for (j = 0; j < n-i-1; j++) 
 	{ 
 		if (arr[j] > arr[j+1]) 
-		{ 
-		swap(&arr[j], &arr[j+1]); 
-		swapped = true; 
+		{ int temp = arr[j];        //Swapping is done if condition is true
+	               arr[j] = arr[j+1]; 
+	               arr[j=1] = temp; 
+		 
+		swapped = true;
 		} 
 	} 
 
